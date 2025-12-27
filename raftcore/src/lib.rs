57683cb62194901126279fe2ac822eb380febd38
Zakir032002 <zakir03032002@gmail.com>
2025-12-27 23:21:12 +0530

    updated raftnode

--- raftcore/src/lib.rs
+++ raftcore/src/lib.rs
@@ -4,7 +4,11 @@ pub mod runtime;
 pub mod events;
 pub mod node;
 pub mod store;
+
 pub use node::RaftNode;
+pub use runtime::RaftRuntime;
+pub use events::Event;
+pub use store::KvStore;
 
 pub use raft::prelude::*;
 pub use raft::storage::MemStorage;
--- raftcore/src/runtime.rs
+++ raftcore/src/runtime.rs
@@ -2,11 +2,45 @@ use crate::RaftNode;
 use crate::events::Event;
 
 use anyhow::Result;
-use tokio::sync::mpsc::{UnboundedReceiver};
+use tokio::sync::mpsc::UnboundedReceiver;
 use tokio::time::{interval, Duration};
-pub struct RaftRuntime{
-      node : RaftNode
-}
-impl RaftRuntime{
 
+pub struct RaftRuntime {
+    pub node: RaftNode,
+}
+
+impl RaftRuntime {
+    pub fn new(node: RaftNode) -> Self {
+        Self { node }
+    }
+
+    pub async fn start(mut self, mut rx: UnboundedReceiver<Event>) -> Result<()> {
+        let mut ticker = interval(Duration::from_millis(100));
+
+        loop {
+            tokio::select! {
+                // Tick every 100ms
+                _ = ticker.tick() => {
+                    self.node.tick();
+                    self.node.on_ready()?;
+                }
+
+                // Receive events
+                Some(event) = rx.recv() => {
+                    match event {
+                        Event::Propose { data, request_id, callback } => {
+                            self.node.callbacks.insert(request_id, callback);
+                            self.node.raw_node.propose(vec![], data)?;
+                        }
+
+                        Event::Step(msg) => {
+                            self.node.raw_node.step(msg)?;
+                        }
+                    }
+
+                    self.node.on_ready()?;
+                }
+            }
+        }
+    }
 }
